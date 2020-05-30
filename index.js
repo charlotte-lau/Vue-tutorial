9d06e47f2570ec5447d5a71af717564a6052d72a
Charlotte <tweety.tinglau@gmail.com>
2020-05-30 13:31:04 +1200

    v-for

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var app = new Vue({
     el: "#app",
     data: {
         message: "Hello Vue!",
-        seen: false
+        seen: false,
+        todos: [
+            {text: "Learn JavaScript"},
+            {text: "Learn Vue"},
+            {text: "Become a web developer"}
+        ]
     }
 });
