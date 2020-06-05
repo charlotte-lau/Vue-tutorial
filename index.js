7771c906168e96a3c2293c061d7b4822b340ca48
Charlotte <tweety.tinglau@gmail.com>
2020-06-05 20:35:44 +1200

    learning how to build a shopping website

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-})
-
 var app = new Vue({
     el: "#app",
     data: {
-        message: "Hello Vue!",
-        seen: false,
-        todos: [
-            {id:0, text: "Learn JavaScript"},
-            {id:1, text: "Learn Vue"},
-            {id:2, text: "Become a web developer"}
-        ]
-    },
-    methods: {
-        reverseMessage: function(){
-            this.message = this.message.split('').reverse().join('')
-        }
+        selected: "green",
+        product:
+            {
+                title: "Socks",
+                details: [
+                    "80% cotton",
+                    "20% polyester",
+                    "Gender-neutral",
+                ],
+                stocks: [
+                    {color:"green", quantity:1, thumbnail:""},
+                    {color:"blue", quantity:0, thumbnail:""},
+                ],
+                image:"./assets/vmSocks-green.jpg",
+                altTxt: "pair of socks"
+            }
+        
     }
 });
