759bdca32e58028397f792a60bb366bf5f8b2749
Charlotte <tweety.tinglau@gmail.com>
2020-06-10 11:31:32 +1200

    use computed variable to control image

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
 var app = new Vue({
     el: "#app",
     data: {
-        selected: "green",
+        selectedVariant: 0,
+        brand: "Charlotte",
         product:
-            {
-                title: "Socks",
-                details: [
-                    "80% cotton",
-                    "20% polyester",
-                    "Gender-neutral",
-                ],
-                variants: [
-                    {color:"green", quantity:1, thumbnail:"./assets/vmSocks-green.jpg", id: 2234},
-                    {color:"blue", quantity:0, thumbnail:"./assets/vmSocks-blue.jpg", id: 2235},
-                ],
-                image:"./assets/vmSocks-green.jpg",
-                altTxt: "pair of socks",
-                inventory: 100
-                
-            },
-            cart: 0
+        {
+            title: "Socks",
+            details: [
+                "80% cotton",
+                "20% polyester",
+                "Gender-neutral",
+            ],
+            variants: [
+                { 
+                    color: "green", 
+                    quantity: 1, 
+                    thumbnail: "./assets/vmSocks-green.jpg", 
+                    id: 2234,
+                    quantity: 10
+                },
+                { 
+                    color: "blue", 
+                    quantity: 0, 
+                    thumbnail: "./assets/vmSocks-blue.jpg", 
+                    id: 2235,
+                    quantity: 0
+                },
+            ],
+            altTxt: "pair of socks",
+            inventory: 100
+
+        },
+        cart: 0
     },
     computed: {
-        inStock: function(){
-            return this.product.inventory>0;
+        inStock: function () {
+            return this.product.variants[this.selectedVariant].quantity > 0;
+        },
+        title: function () {
+            return this.brand + ' ' + this.product.title;
+        },
+        image: function () {
+            return this.product.variants[this.selectedVariant].thumbnail;
         }
     },
     methods: {
-        addToCart:function() {
-            this.cart +=  1;
+        addToCart: function () {
+            this.cart += 1;
         },
-        updateProduct:function(variantImage) {
-            this.product.image = variantImage;
-        }   
+        updateProduct: function (index) {
+            this.selectedVariant = index;
+        }
     }
 });
